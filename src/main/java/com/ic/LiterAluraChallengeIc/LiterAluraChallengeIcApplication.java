d2d927736ab0b5b11935c7321baff25f50e04767
Julieth Caicedo <izuulcaicedo25@gmail.com>
2026-01-27 20:17:21 -0500

    proyecto spring boot inicial

---
+++ src/main/java/com/ic/LiterAluraChallengeIc/LiterAluraChallengeIcApplication.java
@@ -0,0 +1,13 @@
+package com.ic.LiterAluraChallengeIc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraChallengeIcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraChallengeIcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ic/LiterAluraChallengeIc/LiterAluraChallengeIcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ic.LiterAluraChallengeIc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraChallengeIcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
